de86b9921c803da1add40885727b95d1ba2a0cc4
mikhail-belii <sasama15555@gmail.com>
2026-03-24 14:55:52 +0700

    init

---
+++ src/main/java/silverpancake/SilverpancakeApplication.java
@@ -0,0 +1,13 @@
+package silverpancake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverpancakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilverpancakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/silverpancake/SilverpancakeApplicationTests.java
@@ -0,0 +1,13 @@
+package silverpancake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilverpancakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

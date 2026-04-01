690d6c74ccedcccba303dbc86e8b62ad68f016b4
mikhail-belii <sasama15555@gmail.com>
2026-04-01 18:59:57 +0700

    Реализована стратегия рандомного формирования команд

--- src/main/java/silverpancake/application/util/teamformation/strategy/RandomTeamFormation.java
+++ src/main/java/silverpancake/application/util/teamformation/strategy/RandomTeamFormation.java
@@ -18,7 +18,36 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class RandomTeamFormation implements TeamFormation {
+    private final UserTeamRepository userTeamRepository;
+
     @Override
     public void formTeams(Task task, List<Team> teams) {
+        if (teams.isEmpty() || task.getCourse() == null || task.getCourse().getCourseUsers() == null) {
+            return;
+        }
+
+        var shuffledStudents = new ArrayList<>(task.getCourse().getCourseUsers()
+                .stream()
+                .filter(userCourse -> userCourse.getUserRole() == UserCourseRole.STUDENT)
+                .map(UserCourse::getUser)
+                .toList());
+
+        if (shuffledStudents.isEmpty()) {
+            return;
+        }
+
+        Collections.shuffle(shuffledStudents);
+
+        var userTeams = new ArrayList<UserTeam>();
+        for (int i = 0; i < shuffledStudents.size(); i++) {
+            var userTeam = new UserTeam()
+                    .setUser(shuffledStudents.get(i))
+                    .setTeam(teams.get(i % teams.size()))
+                    .setCreatedAt(LocalDateTime.now());
+            userTeams.add(userTeam);
+        }
+
+        userTeamRepository.saveAll(userTeams);
+        userTeamRepository.flush();
     }
 }

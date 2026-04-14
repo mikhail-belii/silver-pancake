76ccfd2231942d0da456ef68ef35f2000d60d88d
Aleksey Zinovjev <alexz04tab2@gmail.com>
2026-04-15 00:44:00 +0700

    TEAMSWAG-DEV-114: Реализована стратегия выбора решения по последнему сданному

---
+++ src/main/java/silverpancake/application/serviceimpl/taskanswer/strategy/LastSolutionStrategy.java
@@ -0,0 +1,35 @@
+package silverpancake.application.serviceimpl.taskanswer.strategy;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import silverpancake.application.mapper.TaskAnswerMapper;
+import silverpancake.application.model.finaltaskanswer.FinalTaskAnswerModel;
+import silverpancake.application.repository.TeamFinalTaskAnswerRepository;
+import silverpancake.application.util.ExceptionUtility;
+import silverpancake.domain.entity.task.Task;
+import silverpancake.domain.entity.taskanswer.TaskAnswer;
+import silverpancake.domain.entity.team.Team;
+
+@Service
+@RequiredArgsConstructor
+public class LastSolutionStrategy implements TaskAttachmentStrategy {
+
+    private final TeamFinalTaskAnswerRepository teamFinalTaskAnswerRepository;
+    private final ExceptionUtility exceptionUtility;
+
+    @Override
+    public FinalTaskAnswerModel process(Team team, Task task, TaskAnswer taskAnswer) {
+        var teamFinalTaskAnswer = teamFinalTaskAnswerRepository.findByTaskIdAndTeamId(task.getId(), team.getId())
+                .orElseThrow(exceptionUtility::teamFinalTaskAnswerNotFoundException);
+
+        teamFinalTaskAnswer.setFinalTaskAnswer(taskAnswer);
+        teamFinalTaskAnswer = teamFinalTaskAnswerRepository.save(teamFinalTaskAnswer);
+
+        return TaskAnswerMapper.toModel(teamFinalTaskAnswer);
+    }
+
+    @Override
+    public TaskAnswerFinalizationType getType() {
+        return TaskAnswerFinalizationType.LAST_ATTACHMENT;
+    }
+}
